9497e02406bba344f8333a82787d751dd3c238e4
githubprash <prashantbj.30@gmail.com>
2022-07-04 15:01:06 +0530

    Initialising

---
+++ src/main/java/com/example/piramal/piramalpro/PiramalproApplication.java
@@ -0,0 +1,13 @@
+package com.example.piramal.piramalpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiramalproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiramalproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/piramal/piramalpro/PiramalproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.piramal.piramalpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiramalproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
